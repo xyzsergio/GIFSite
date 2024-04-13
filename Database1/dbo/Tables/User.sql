65140c2f35ee73de2572c34d0d4b3857ee989e09
Giovanni Smith <51383214+GiovanniSmith@users.noreply.github.com>
2024-04-13 16:53:46 -0500

    Added AdminID to user table

--- Database1/dbo/Tables/User.sql
+++ Database1/dbo/Tables/User.sql
@@ -1,11 +1,16 @@
 ﻿CREATE TABLE [dbo].[User] (
-    [UserID]   INT           IDENTITY (1, 1) NOT NULL,
-    [Username] VARCHAR (50)  NOT NULL,
-    [Email]    VARCHAR (100) NOT NULL,
-    [Password] VARCHAR (100) NOT NULL,
-    [LastLoginTime] DATETIME NULL, 
+    [UserID]        INT           IDENTITY (1, 1) NOT NULL,
+    [Username]      VARCHAR (50)  NOT NULL,
+    [Email]         VARCHAR (100) NOT NULL,
+    [Password]      VARCHAR (100) NOT NULL,
+    [LastLoginTime] DATETIME      NOT NULL,
+    [AdminID]       INT           NOT NULL,
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC)
 );
+
+
+
+
 
 
 
