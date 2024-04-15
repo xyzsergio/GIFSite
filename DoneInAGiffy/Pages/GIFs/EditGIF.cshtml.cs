1ff3574a7ccf2dce119c8c06b818b27693182705
Giovanni Smith <51383214+GiovanniSmith@users.noreply.github.com>
2024-04-14 22:34:37 -0500

    Add/Edit/View menu items video - completed

--- DoneInAGiffy/Pages/GIFs/EditGIF.cshtml.cs
+++ DoneInAGiffy/Pages/GIFs/EditGIF.cshtml.cs
@@ -1,16 +1,66 @@
 using DoneInAGiffy.Pages.Model;
+using GIFLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace DoneInAGiffy.Pages.GIFs
 {
     [BindProperties]
     public class EditGIFModel : PageModel
     {
-        public GIF Item { get; set; } = new GIF();
+        public GIF GifItem { get; set; } = new GIF();
         
         public void OnGet(int id)
         {
+            PopulateGIF(id);
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+                {
+                    string cmdText = "UPDATE GIF SET Title=@title, Description=@description, UploadDate=@uploaddate, FilePath=@filepath " + 
+                        "WHERE gifID=@gifID";
+                    SqlCommand cmd = new SqlCommand(cmdText, conn);
+                    cmd.Parameters.AddWithValue("@title", GifItem.gifTitle);
+                    cmd.Parameters.AddWithValue("@description", GifItem.gifDescription);
+                    cmd.Parameters.AddWithValue("@uploaddate", GifItem.gifUploadDate);
+                    cmd.Parameters.AddWithValue("@filepath", GifItem.gifFilePath);
+                    cmd.Parameters.AddWithValue("@gifID", id);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    return RedirectToPage("ViewGIFs");
+                }
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        private void PopulateGIF(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                string cmdText = "SELECT gifID, Title, Description, UploadDate, FilePath from GIF WHERE gifID=@gifid";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@itemId", id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    GifItem.gifID = id;
+                    GifItem.gifTitle = reader.GetString(1);
+                    GifItem.gifDescription = reader.GetString(2);
+                    GifItem.gifUploadDate = reader.GetDateTime(3);
+                    GifItem.gifFilePath = reader.GetString(4);
+                }
+            }
         }
     }
 }
