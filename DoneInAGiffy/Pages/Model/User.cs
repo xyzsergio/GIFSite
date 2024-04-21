ceb1abc540db3835b0924b6c97f65f1a4984fdbc
Brye Hufschmidt <hufschmibe@jacks.sfasu.edu>
2024-04-21 16:05:20 -0500

    Password requires 10 characters

--- DoneInAGiffy/Pages/Model/User.cs
+++ DoneInAGiffy/Pages/Model/User.cs
@@ -14,6 +14,8 @@ namespace DoneInAGiffy.Pages.Account.Model
 
         public string Email { get; set; }
         [Required(ErrorMessage = "The password is required.")]
+        [StringLength(100, ErrorMessage = "The password must be at least 10 characters long.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
         [Display(Name = "Password: ")]
         public string Password { get; set; }
     }
