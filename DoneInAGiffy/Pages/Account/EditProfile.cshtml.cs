192bb33c27f013f002d94bedf71a456c9066c469
Sergio Sauceda <xyzsergio12@gmail.com>
2024-05-09 18:20:51 -0500

    in progress

--- DoneInAGiffy/Pages/Account/EditProfile.cshtml.cs
+++ DoneInAGiffy/Pages/Account/EditProfile.cshtml.cs
@@ -12,43 +12,18 @@ namespace DoneInAGiffy.Pages.Account
     {
         public UserProfile profile { get; set; } = new UserProfile();
 
-        public void OnGet()
+        public void OnGet(int itemid)
         {
             PopulateProfile();
         }
 
-        public void OnPost()
-        {
-            UpdateProfile();
-        }
-
-        private void PopulateProfile()
-        {
-            string email = HttpContext.User.FindFirstValue(ClaimTypes.Email);
-            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
-            {
-                string cmdText = "SELECT Username, Email, ProfilePictureLink, ProfileDescription FROM [User] WHERE Email=@email";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-                cmd.Parameters.AddWithValue("@email", email);
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if (reader.Read())
-                {
-                    profile.Username = reader.GetString(0);
-                    profile.Email = reader.GetString(1);
-                    profile.ProfilePictureLink = reader.IsDBNull(2) ? null : reader.GetString(2);
-                    profile.ProfileDescription = reader.IsDBNull(3) ? null : reader.GetString(3);
-                }
-            }
-        }
-
-        private IActionResult UpdateProfile()
+        public IActionResult OnPost(int itemid)
         {
             if (ModelState.IsValid)
             {
                 using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
                 {
-                    string cmdText = "UPDATE [User] SET Username=@username, Email=@email, ProfilePictureLink=@profilePictureLink, ProfileDescription=@profileDescription WHERE Email=@email";
+                    string cmdText = "UPDATE [User] SET Username=@username, Email=@email, ProfilePictureLink=@profilePictureLink, ProfileDescription=@profileDescription WHERE UserID=@itemId";
                     SqlCommand cmd = new SqlCommand(cmdText, conn);
                     cmd.Parameters.AddWithValue("@username", profile.Username);
                     cmd.Parameters.AddWithValue("@email", profile.Email);
@@ -70,6 +45,26 @@ namespace DoneInAGiffy.Pages.Account
                     }
                 }
                 return Page();
+            }
+        }
+
+        private void PopulateProfile()
+        {
+            string email = HttpContext.User.FindFirstValue(ClaimTypes.Email);
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                string cmdText = "SELECT Username, Email, ProfilePictureLink, ProfileDescription FROM [User] WHERE Email=@email";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@email", email);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    profile.Username = reader.GetString(0);
+                    profile.Email = reader.GetString(1);
+                    profile.ProfilePictureLink = reader.IsDBNull(2) ? null : reader.GetString(2);
+                    profile.ProfileDescription = reader.IsDBNull(3) ? null : reader.GetString(3);
+                }
             }
         }
     }
